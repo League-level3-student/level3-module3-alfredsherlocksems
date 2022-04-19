ffc857077471611117221198fe328e86e1d1221f
student <student@ip-172-31-50-209.us-west-2.compute.internal>
2022-04-18 17:00:47 -0700

    string methodas

--- src/_00_Intro_To_String_Methods/_01_StringMethods.java
+++ src/_00_Intro_To_String_Methods/_01_StringMethods.java
@@ -34,13 +34,19 @@ public class _01_StringMethods {
 
     // Given Strings s1 and s2, return the longer String
     public static String longerString(String s1, String s2) {
-        return null;
+        if (s1.length() > s2.length()) {
+        	return s1;
+        }
+        return s2;
     }
 
     // If String s contains the word "underscores", change all of the spaces
     // to underscores
     public static String formatSpaces(String s) {
-        return null;
+       if (s.contains("underscores")) {
+    	   s = s.replace(' ', '_');
+       }
+        return s;
     }
 
     // Return the name of the person whose LAST name would appear first if they
@@ -48,28 +54,106 @@ public class _01_StringMethods {
     // You cannot assume there are no extra spaces around the name, but you can
     // assume there is only one space between the first and last name
     public static String lineLeader(String s1, String s2, String s3) {
-        return null;
+    	char[] l1 = new char[3];
+    	char one;
+    	char two;
+    	char three;
+    	int a1 = 0;
+    	int a2 = 0;
+    	int a3 = 0;
+    	int c = 0;
+    	String r = "";
+    	for (int i = 0; i < s1.length(); i++) {
+        	if (!(s1.charAt(i) == ' ')) {
+        		l1[0] = s1.charAt(i);
+        		one = l1[0];
+        		a1 = i;
+        		break;
+        	}
+        }
+    	for (int i = 0; i < s2.length(); i++) {
+        	if (!(s2.charAt(i) == ' ')) {
+        		l1[1] = s2.charAt(i);
+        		two = l1[1];
+        		a2 = i;
+        		break;
+        	}
+        }
+    	for (int i = 0; i < s3.length(); i++) {
+    		if (!(s3.charAt(i) == ' ')) {
+        		l1[2] = s3.charAt(i);
+        		three = l1[2];
+        		a3 = i;
+        		break;
+        	}
+		}
+    	for (int i = 0; i < l1.length; i++) {
+    		for (int k = 0; k < l1.length - 1; k++) {
+    			if (l1[k+1] > l1[k]) {
+    				char t = l1[k];
+    				l1[k] = l1[k+1];
+    				l1[k+1] = t;
+    			}
+    		}
+    	}
+    	if(l1[0] == one) {
+    		for (int i = a1; i < s1.length(); i++) {
+    			if (s1.charAt(i) == ' ') {
+    				c = i;
+    				break;
+    			}
+    		}
+    		r = s1.substring(a1, c);
+    	}
+    	else if (l1[1] == two) {
+    		for (int i = a2; i < s2.length(); i++) {
+    			if (s2.charAt(i) == ' ') {
+    				c = i;
+    				break;
+    			}
+    		}
+    		r = s2.substring(a2, c);
+    	}
+    	else {
+    		for (int i = a3; i < s3.length(); i++) {
+    			if (s3.charAt(i) == ' ') {
+    				c = i;
+    				break;
+    			}
+    		}
+    		r = s3.substring(a3, c);
+    	}
+    	return r;
     }
 
     // Return the sum of all numerical digits in the String
     public static int numeralSum(String s) {
-        return 0;
+        int st = Integer.parseInt(s);
+        return st;
     }
 
     // Return the number of times String substring appears in String s
     public static int substringCount(String s, String substring) {
-        return 0;
+        int l = substring.length();
+        int num = 0;
+        for (int i = 0; i< s.length() - l + 1; i++) {
+        	String s2 = s.substring(i, l + i);
+        	if (s2.contains(substring)) {
+        		num++;
+        	}
+        }
+    	return num;
     }
 
     // Call Utilities.encrypt at the bottom of this file to encrypt String s
     public static String encrypt(String s, char key) {
-        return null;
+        return encrypt(s, key);
     }
 
     // Call Utilities.decrypt at the bottom of this file to decrypt the
     // cyphertext (encrypted text)
     public static String decrypt(String s, char key) {
-        return null;
+        return decrypt(s, key);
     }
 
     // Return the number of words in String s that end with String substring
